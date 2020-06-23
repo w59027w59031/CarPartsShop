808277c697b6026940dcbc6130b00b126f5b4e43
PaNATO <chief1097@gmail.com>
2020-06-23 22:32:47 +0200

    Revert "Revert "Aplikacja Ver. 1.0.0""
    This reverts commit b0b494dd227f21463942aef99f6ae88ea323580a.

--- car-parts-shop/src/components/NavigationBar.vue
+++ car-parts-shop/src/components/NavigationBar.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="navigationBar">
 
-<div class="pos-f-t">  
+<div class="pos-f-t">
   <nav class="navbar-header navbar navbar-dark bg-dark">
     <div>
-      <router-link to="/Start" class="nav-link-header float-left navbar-brand">Start</router-link>
+      <router-link to="/" class="nav-link-header float-left navbar-brand">Start</router-link>
       <router-link to="/Register" class="nav-link-header float-left nav-link">Register</router-link>
 
     </div>
@@ -15,7 +15,7 @@
       <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Zaloguj</button>
     </form>
     <form class="form-inline">
-      <button class="btn btn-outline-success mr-sm-2" type="">
+      <router-link to="/Basket"><button class="btn btn-outline-success mr-sm-2" type="">
         <svg class="bi bi-basket" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
           <path fill-rule="evenodd" d="M10.243 1.071a.5.5 0 0 1 .686.172l3 5a.5.5 0 1 1-.858.514l-3-5a.5.5 0 0 1 .172-.686zm-4.486 0a.5.5 0 0 0-.686.172l-3 5a.5.5 0 1 0 .858.514l3-5a.5.5 0 0 0-.172-.686z"/>
           <path fill-rule="evenodd" d="M1 7v1h14V7H1zM.5 6a.5.5 0 0 0-.5.5v2a.5.5 0 0 0 .5.5h15a.5.5 0 0 0 .5-.5v-2a.5.5 0 0 0-.5-.5H.5z"/>
@@ -23,10 +23,10 @@
           <path fill-rule="evenodd" d="M4 10a.5.5 0 0 1 .5.5v3a.5.5 0 1 1-1 0v-3A.5.5 0 0 1 4 10zm2 0a.5.5 0 0 1 .5.5v3a.5.5 0 1 1-1 0v-3A.5.5 0 0 1 6 10zm2 0a.5.5 0 0 1 .5.5v3a.5.5 0 1 1-1 0v-3A.5.5 0 0 1 8 10zm2 0a.5.5 0 0 1 .5.5v3a.5.5 0 1 1-1 0v-3a.5.5 0 0 1 .5-.5zm2 0a.5.5 0 0 1 .5.5v3a.5.5 0 1 1-1 0v-3a.5.5 0 0 1 .5-.5z"/>
         </svg>
       </button>
+      </router-link>
       <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Wyloguj</button>
     </form>
 
-    
   </nav>
 </div>
   </div>
@@ -35,8 +35,10 @@
 <script>
 export default {
   name: 'NavigationBar',
-  data(){
-    return{msga:"tests",}
-  }
+  data() {
+    return {
+      msga: 'tests',
+    };
+  },
 };
 </script>
--- car-parts-shop/src/main.js
+++ car-parts-shop/src/main.js
@@ -1,15 +1,17 @@
 import Vue from 'vue';
 import App from './App.vue';
-import { BootstrapVue, IconsPlugin } from '../node_modules/bootstrap-vue'
+import { BootstrapVue, IconsPlugin } from '../node_modules/bootstrap-vue';
+import '../node_modules/bootstrap/dist/css/bootstrap.css';
+import '../node_modules/bootstrap-vue/dist/bootstrap-vue.css';
+import './assets/app.css';
+import router from './router';
 
-Vue.use(BootstrapVue)
-Vue.use(IconsPlugin)
+Vue.use(BootstrapVue);
+Vue.use(IconsPlugin);
 
-import '../node_modules/bootstrap/dist/css/bootstrap.css'
-import '../node_modules/bootstrap-vue/dist/bootstrap-vue.css'
-import './assets/app.css'
 Vue.config.productionTip = false;
 
 new Vue({
   render: (h) => h(App),
+  router,
 }).$mount('#app');
